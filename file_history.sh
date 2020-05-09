df30695c7458d8995615c4a2af5ac540114d7f8d
Marcus Russi <marcusrussi@gmail.com>
2020-05-08 23:52:55 -0400

    All work from Friday night coding rampage

---
+++ file_history.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# This program prints every revision of the file passed as the first argument.
+# Each line of every commit of the file is printed, with the date of the commit
+# appended to the file. The date field is printed as ',YYYY-MM-DD'.
+#
+# GNU Parallel is required in order for this script to work. Furthermore, the 
+# file must be version-controlled by Git.
+#
+# To customize the modifications made by the script, refer to documentation for
+# the following commands:
+#
+# - git submodule
+# - git cat-file
+# - git log
+
+# The file we want to see the revisions of
+TARGET_FILE=`basename "$1"`
+ENCLOSING_DIR=`dirname "$1"`
+OLD_WD=`pwd`
+
+# Go into the submodule, so that arguments to 'git cat-file' won't be
+# garbled
+cd "$ENCLOSING_DIR"
+
+# Print a list of every {hash},{YYYY-MM-DD} of this file
+# Since the above ^ is technically a .csv file, treat it as such
+# Then, for each line, print the file associated with that commit hash
+# Delete its first line, since that is merely a header, and append the date
+# of the commit as the final column in the "csv"
+git log --pretty='%h,%at' -- $TARGET_FILE | \
+  parallel --csv \
+    git cat-file -p '{1}:'"$TARGET_FILE" '|' \
+    sed -e '1d' -e 's/$/,{2}/' '|' \
+    sed -e "'\$a\'"
+
+cd "$OLD_WD"
---
+++ sketch.R
@@ -0,0 +1,143 @@
+library(tidyverse)
+library(anytime)
+library(glue)
+
+file_name <- "us-counties-aggregated.csv"
+
+c(
+  'date', 'county', 'state', 'fips',
+  'cases', 'deaths', 'date_commit'
+) -> col_names
+
+cols(
+  date        = col_date(format = '%Y-%m-%d'), # YYYY-MM-DD format
+  county      = col_character(),
+  state       = col_character(),
+  fips        = col_number(),
+  cases       = col_number(),
+  deaths      = col_number(),
+  date_commit = col_number() # unix timestamp
+) -> col_types
+
+transform_incidence <- function(d) {
+  group_by(d, date_commit, county, state) %>%
+    arrange(date) %>%
+    mutate(cases  = cases  - lag(cases,  default = 0),
+           deaths = deaths - lag(deaths, default = 0)) %>%
+    ungroup
+}
+
+p <- read_csv(file_name, col_names = col_names, col_types = col_types)
+
+prevalence <- mutate(d, date_commit = anytime(date_commit))
+incidence  <- transform_incidence(prevalence)
+
+group_by(incidence, date, county, state) %>%
+  arrange(date_commit) %>%
+  mutate(cases  = cases  - lag(cases),
+         deaths = deaths - lag(deaths)) %>%
+  ungroup() %>%
+  arrange(date, date_commit) -> deltas
+
+plot_deltas <- function(d, state_ = NULL, county_ = NULL) {
+
+  predicates <- quos()
+
+  if (!is.null(state_))
+    predicates <- quos(state == state_)
+
+  if (!is.null(county_) && !is.null(state_))
+    predicates <- quos(county == county_)
+  
+  if (!is.null(county_) && is.null(state_))
+    stop("You specified a county, but not a state")
+
+  filtered_data <- filter(d, !!! predicates)
+
+  summed_data <- group_by(filtered_data, date, date_commit) %>%
+    summarize_at(vars(cases, deaths), sum) %>%
+    ungroup
+
+  ggplot(
+    summed_data,
+    aes(date, date_commit, fill = cases > 0)
+  ) +
+  geom_tile(height = 12*60^2) +
+  scale_fill_manual(
+    values = c("green", "red")
+  ) +
+  theme_minimal()
+}
+
+plot_discrepancy <- function(d,
+                             county_ = NULL,
+                             state_  = NULL) {
+
+  predicates <- quos()
+
+  if (!is.null(state_))
+    predicates <- quos(state == state_)
+
+  if (!is.null(county_) && !is.null(state_))
+    predicates <- quos(county == county_)
+  
+  if (!is.null(county_) && is.null(state_))
+    stop("You specified a county, but not a state")
+
+  filtered_data <- filter(d, !!! predicates)
+
+  summed_data <- group_by(filtered_data, date, date_commit) %>%
+    summarize_at(vars(cases, deaths), sum) %>%
+    ungroup
+
+  ggplot(summed_data, aes(x = date, group = date_commit)) +
+    geom_line(
+      color = 'grey',
+      aes(y = deaths)
+    ) +
+    geom_point(
+      data = ~group_by(., date_commit) %>% top_n(1, date),
+      aes(y = deaths, color = date_commit)
+    ) +
+    geom_line(
+      color = 'grey',
+      aes(y = cases)
+    ) +
+    geom_point(
+      data = ~group_by(., date_commit) %>% top_n(1, date),
+      aes(y = cases, color = date_commit)
+    ) +
+    geom_text(
+      data = ~top_n(., 1, date),
+      aes(y = cases),
+      hjust = 1, nudge_x = -0.10, nudge_y = -0.10, angle = 45,
+      label = "Cases"
+    ) +
+    geom_text(
+      data = ~top_n(., 1, date),
+      aes(y = deaths),
+      hjust = 1, nudge_x = -0.10, nudge_y = -0.10, angle = 45,
+      label = "Deaths"
+    ) +
+    scale_y_continuous(
+      trans = scales::pseudo_log_trans(base = 10),
+      labels = scales::label_number_si(),
+      breaks = function(limits)
+        c(0, scales::breaks_log(n=8)(c(1, limits[2])))
+    ) +
+    scale_x_date(date_breaks = '1 week',
+                 date_labels = "%b %d",
+                 minor_breaks = NULL) +
+    annotation_logticks() +
+    theme_linedraw() +
+    labs(
+      x = "Date referenced",
+      y = "Quantity",
+      color = "Date committed"
+    ) +
+    theme(
+      legend.justification = c(1,0),
+      legend.position = c(1,0),
+      legend.box = 'horizontal'
+    )
+}
