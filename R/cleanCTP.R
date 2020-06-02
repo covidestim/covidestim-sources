e80408d25b20a4a9e44cf6bcf937df5ec4c5ab73
Marcus Russi <marcusrussi@gmail.com>
2020-06-02 13:12:19 -0400

    Replaced old CTP cleaning script

--- R/cleanCTP.R
+++ R/cleanCTP.R
@@ -1,33 +1,35 @@
-#!/usr/bin/env Rscript
-
-# dataframe "daily_cases_deaths_fractionPositive" with date, state, daily cases, daily deaths, and smoothened daily fraction positive ~ line 75
-
 library(lubridate, warn.conflicts = FALSE)
-library(ggplot2)
-library(readr)
-library(reshape2)
-suppressPackageStartupMessages(library(openintro))
-library(docopt)
-library(magrittr)
-
+library(ggplot2,   warn.conflicts = FALSE)
+library(reshape2,  warn.conflicts = FALSE)
+suppressPackageStartupMessages( library(openintro, warn.conflicts = FALSE) )
+library(dplyr,     warn.conflicts = FALSE)
+library(ggpubr,    warn.conflicts = FALSE)
+library(gridExtra, warn.conflicts = FALSE)
+library(readr,     warn.conflicts = FALSE)
+library(docopt,    warn.conflicts = FALSE)
+library(magrittr,  warn.conflicts = FALSE)
+library(cli,       warn.conflicts = FALSE)
+                    
 'CTP Cleaner
 
 Usage:
-  cleanCTP.R -o <path> [--save-graphs <path>] <path>
+  cleanCTP.R -o <path> [--graphs <path>] <path>
   cleanCTP.R (-h | --help)
   cleanCTP.R --version
 
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  -o            Path to output cleaned data to.
+  -o <path>             Path to output cleaned data to.
+  --graphs <path>       File to save .pdf of data-related figures to
+  -h --help             Show this screen.
+  --version             Show version.
 
 ' -> doc
 
 arguments   <- docopt(doc, version = 'CTP Cleaner 0.1')
-input_path  <- arguments$path[3]
-output_path <- arguments$path[1]
-graphs_path <- arguments$path[2]
+
+input_path  <- arguments$path
+output_path <- arguments$o
+graphs_path <- arguments$graphs
 
 cols_only(
   date                     = col_date(format = '%Y%m%d'),
@@ -58,112 +60,303 @@ cols_only(
   totalTestResultsIncrease = col_number()
 ) -> col_types.covidtracking
 
-# CTP <- "https://raw.githubusercontent.com/COVID19Tracking/covid-tracking-data/master/data/states_daily_4pm_et.csv"
-CTP <- read_csv(input_path, col_types = col_types.covidtracking)
+cli_alert_info("Loading CTP data from {.file {input_path}}")
+cli_process_start("Loading CTP data from {.file {input_path}}")
+CTP_loaded <- read_csv(input_path, col_types = col_types.covidtracking)
+cli_process_done()
 
-# The data at the top of the file are the most recent dates
-CTP_data <- CTP[nrow(CTP):1,]
+# Reverse dataframe
+CTP_data <- CTP_loaded[nrow(CTP_loaded):1, ]
 
-# lubridate
+# Lubridate date
 CTP_data$date <- ymd(CTP_data$date)
 
-# replace the state abbreviations with state names
-CTP_data$state[which(CTP_data$state=="AS")] <- "American Samoa"
-CTP_data$state[which(CTP_data$state=="PR")] <- "Puerto Rico"
-CTP_data$state[which(CTP_data$state=="GU")] <- "Guam"
-CTP_data$state[which(CTP_data$state=="VI")] <- "Virgin Islands"
-CTP_data$state[which(CTP_data$state=="MP")] <- "Northern Mariana Islands"
-CTP_data$state[which(CTP_data$state!="American Samoa"&CTP_data$state!="Puerto Rico"&CTP_data$state!="Guam"&CTP_data$state!="Virgin Islands"&CTP_data$state!="Northern Mariana Islands")] <- abbr2state(CTP_data$state[(CTP_data$state!="American Samoa"&CTP_data$state!="Puerto Rico"&CTP_data$state!="Guam"&CTP_data$state!="Virgin Islands"&CTP_data$state!="Northern Mariana Islands")])
+c(
+  "AS" = "American Samoa",
+  "PR" = "Puerto Rico",
+  "GU" = "Guam",
+  "VI" = "Virgin Islands",
+  "MP" = "Northern Mariana Islands"
+) -> excluded_states
 
+CTP_data <- dplyr::filter(CTP_data, ! state %in% names(excluded_states))
+CTP_data <- dplyr::mutate(CTP_data, state = abbr2state(state))
 state_names <- unique(CTP_data$state)
 
-# initialize single dataframe => date, state, daily positive (dp), daily increase in tests (dt), daily fraction postiive smoothed (df), daily deaths
+##################################
+##                              ##
+##   Clean & Prepare the data   ##
+##                              ##
+##################################
 
-CTP_movingAverages <- data.frame(date=as.Date(character()),state=character(),cum_cases=double(),cum_totalTests=double(),cum_deaths=double(),daily_cases=double(),daily_totalTests=double(),daily_deaths=double(),cum_casesFilled=double(),cum_deathsFilled=double(),cum_totalTestsFilled=double(),daily_cases_9dayMovingAverage=double(),daily_totalTests_9dayMovingAverage=double(),daily_fractionPositive=double(),daily_fractionPositiveSmoothened=double(),stringsAsFactors = F)
+# Initialize a dataframe to hold all the results
+CTP_cleaned <-
+  data.frame(
+    date                                  = as.Date(character()),
+    state                                 = character(),
+    cum_cases                             = double(),
+    cum_totalTests                        = double(),
+    cum_deaths                            = double(),
+    daily_casesFilled                     = double(),
+    daily_deathsFilled                    = double(),
+    daily_fractionPositive                = double(),
+    daily_fractionPositive_15dayMovingAvg = double(),
+    stringsAsFactors                      = FALSE
+  )
 
-# for loop to add incident cases, incident deaths, and rbind dataset 
-for(i in 1:length(state_names)){
+# Iterate through each state to clean data and add smoothed fraction positive
+cli_process_start("Applying moving average to states")
+for (i in 1:length(state_names)) {
+
+  c(
+    "date"             = "date",
+    "state"            = "state",
+    "positive"         = "cum_cases",
+    "totalTestResults" = "cum_totalTests",
+    "death"            = "cum_deaths"
+  ) -> name_mapping
   
-  temp_data <- CTP_data[which(CTP_data$state==state_names[i]),]
-  temp_data <- temp_data[,c("date","state","positive","totalTestResults","death")]
-  colnames(temp_data) <- c("date","state","cum_cases","cum_totalTests","cum_deaths")
+  # Initialize a temporary dataframe
+  temp_data           <- CTP_data[which(CTP_data$state == state_names[i]), ]
+  temp_data           <- temp_data[, names(name_mapping)]
+  colnames(temp_data) <- name_mapping
   
-  # add zero to cumulative cases
-  cum_cases <- c(0,temp_data$cum_cases)
-  cum_deaths <- c(0,temp_data$cum_deaths)
-  cum_totalTests <- c(0,temp_data$cum_totalTests)
+  # For days in which the daily difference is below zero (for example, a data
+  # audit resulting in a downward revision of the cumulative count), we adjust
+  # the difference of that and subsequent days to zero until the cumulative
+  # count rises above the previous maximum cumulative count, such that the
+  # cumulative count increasesd  monotonically.
+  cum_cases  <- c(0, temp_data$cum_cases)
+  cum_deaths <- c(0, temp_data$cum_deaths)
+  for (i in 2:length(cum_cases)) {
+    cum_cases[i]  <- max(cum_cases[1:i], na.rm = T)
+    cum_deaths[i] <- max(cum_deaths[1:i], na.rm = T)
+  }
+  temp_data$daily_casesFilled  <- diff(cum_cases)
+  temp_data$daily_deathsFilled <- diff(cum_deaths)
+
+  ####################
+  ##                ##
+  ## MOVING AVERAGE ##
+  ##                ##
+  ####################
   
-  # fill in cumulative cases
-  for(i in 2:length(cum_cases)){
-    cum_cases[i] <- max(cum_cases[1:i],na.rm = T)
-    cum_deaths[i] <- max(cum_deaths[1:i],na.rm = T)
-    cum_totalTests[i] <- max(cum_totalTests[1:i],na.rm=T)
+  # calculate the crude daily incidence, total teaths, and deaths from 
+  # cumulative data
+  daily_cases      <- diff(c(0, temp_data$cum_cases))
+  daily_totalTests <- diff(c(0, temp_data$cum_totalTests))
+
+  # Prepare positives by censoring counts from days in which daily cases
+  # are less than 1
+  daily_cases[which(daily_cases < 0)] <- NA
+  
+  # Prepare total tests by censoring counts from days in which daily total tests
+  # are less than 1 or if daily cases are greater than daily total tests
+  problem_indices <- which(
+    daily_totalTests < 0 |
+      (daily_cases > daily_totalTests)
+  )
+  daily_totalTests[problem_indices] <- NA
+
+  # Compute the 15-day moving average
+  daily_case_15dayMovingAvg       <- rep(NA, nrow(temp_data))
+  daily_totalTests_15dayMovingAvg <- rep(NA, nrow(temp_data))
+  for (k in 1:nrow(temp_data)) {
+    startIdx <- max(1, k - 7)
+    endIdx   <- min(nrow(temp_data), k + 7)
+    Idxs     <- startIdx:endIdx
+    daily_case_15dayMovingAvg[k]       <- mean(daily_cases[Idxs], na.rm=T)
+    daily_totalTests_15dayMovingAvg[k] <- mean(daily_totalTests[Idxs], na.rm=T)
   }
   
-  # calculate daily
-  temp_data$daily_cases <- diff(cum_cases)
-  temp_data$daily_deaths <- diff(cum_deaths)
-  temp_data$daily_totalTests <- diff(cum_totalTests)
-  
-  # save filled cum cases
-  temp_data$cum_casesFilled <- cum_cases[-1]
-  temp_data$cum_deathsFilled <- cum_deaths[-1]
-  temp_data$daily_totalTestsFilled <- cum_totalTests[-1]
-  
-  # create moving averages
-  temp_data$daily_cases_9dayMovingAverage <- rep(NA,nrow(temp_data))
-  temp_data$daily_totalTests_9dayMovingAverage <- rep(NA,nrow(temp_data))
-  
-  # 7 day moving averages
-  for(k in 5:(nrow(temp_data)-4)){
-    temp_data$daily_cases_9dayMovingAverage[k] <- mean(temp_data$daily_cases[(k-4):(k+4)])
-    temp_data$daily_totalTests_9dayMovingAverage[k] <- mean(temp_data$daily_totalTests[(k-4):(k+4)])
+  # Calculate raw fraction positive
+  temp_data$daily_fractionPositive <-
+    daily_cases / daily_totalTests
+
+  # Calculate smooth fraction positive from moving average
+  temp_data$daily_fractionPositive_15dayMovingAvg <-
+    daily_case_15dayMovingAvg / daily_totalTests_15dayMovingAvg
+
+  # For any days in which the fifteen-day moving average of positive tests was
+  # is greater than or equal to the fifteen-day moving average of total tests,
+  # we use the fraction positive computed from the previous days' moving
+  # averages, allowing us to accommodate data from states that report negative
+  # tests on a weekly or biweekly basis. For the first
+  # day, if the moving avg. of cases is greater than or equal to the moving avg
+  # of the total number of tests, set the fraction positive to the max value of
+  # 0.9.
+  temp_data$daily_fractionPositive_15dayMovingAvg[1] <-
+    ifelse(
+      daily_case_15dayMovingAvg[1] >=
+        daily_totalTests_15dayMovingAvg[1],
+      0.9,
+      temp_data$daily_fractionPositive_15dayMovingAvg[1]
+    )
+  for (j in 2:(nrow(temp_data))) {
+    if (daily_case_15dayMovingAvg[j] >=
+        daily_totalTests_15dayMovingAvg[j]) {
+
+      temp_data$daily_fractionPositive_15dayMovingAvg[j] <-
+        temp_data$daily_fractionPositive_15dayMovingAvg[j - 1]
+    }
   }
   
-  temp_data$daily_fractionPositive <- temp_data$daily_cases/temp_data$daily_totalTests
-  temp_data$daily_fractionPositiveSmoothened <- temp_data$daily_cases_9dayMovingAverage/temp_data$daily_totalTests_9dayMovingAverage
+  # Cap the moving average at 0.9
+  temp_data$daily_fractionPositive_15dayMovingAvg <-
+    ifelse(
+      temp_data$daily_fractionPositive_15dayMovingAvg > 0.90,
+      0.9,
+      temp_data$daily_fractionPositive_15dayMovingAvg
+    )
+
+  #####################
+  ##                 ##
+  ## BUILD DATAFRAME ##
+  ##                 ##
+  #####################
+
+  CTP_cleaned <- rbind(CTP_cleaned, temp_data)
   
-  CTP_movingAverages <- rbind(CTP_movingAverages,temp_data)
-  remove(temp_data)
+  rm(
+    name_mapping, temp_data, cum_cases, cum_deaths, daily_cases,
+    daily_totalTests, problem_indices, startIdx, endIdx, Idxs,
+    daily_case_15dayMovingAvg, daily_totalTests_15dayMovingAvg
+  )
   
 }
+cli_process_done()
 
-daily_cases_deaths_fractionPositive <- CTP_movingAverages[,c("date","state","daily_cases","daily_deaths","daily_fractionPositiveSmoothened")]
+###################
+##               ##
+## WRITE THE csv ##
+##               ##
+###################
 
-dplyr::rename(
-  daily_cases_deaths_fractionPositive,
-  cases = daily_cases,
-  deaths = daily_deaths,
-  fracpos = daily_fractionPositiveSmoothened
-) %>% dplyr::mutate(
-  fracpos = ifelse(is.nan(fracpos), 0, fracpos)
-) %>% tidyr::replace_na(list(fracpos = 0)) -> final
+# The variables that will be part of the cleaned data
+c(
+  "date"                                  = "date",
+  "state"                                 = "state",
+  "daily_casesFilled"                     = "cases",
+  "daily_deathsFilled"                    = "deaths",
+  "daily_fractionPositive_15dayMovingAvg" = "fracpos"
+) -> final_vars
 
-write_csv(final, output_path)
+# The final CTP data frame             
+final.df <- CTP_cleaned[, names(final_vars)]
+colnames(final.df) <- final_vars
 
-## Visualize the data
+write_csv(final.df, output_path)
+cli_alert_success("Wrote cleaned data to {.file {output_path}}")
 
-CTP_daily_cases_tests <- CTP_movingAverages[,c("date","state","daily_cases","daily_totalTests")]
-CTP_daily_cases_tests_long <- melt(CTP_daily_cases_tests,id.vars=c("date","state"))
+if (is.null(graphs_path))
+  quit()
 
-CTP_fractionPositive <- CTP_movingAverages[,c("date","state","daily_fractionPositive","daily_fractionPositiveSmoothened")]
-CTP_fractionPositive_long <- melt(CTP_fractionPositive,id.vars=c("date","state"))
+########################
+##                    ##
+## Visualize the data ##
+##                    ##
+########################
+cli_process_start("Producing graphs")
 
-# view daily cases and tests
-ggplot(data=CTP_daily_cases_tests_long,aes(x=date,y=value,color=variable))+
-  geom_line()+
-  facet_wrap(~state, scales="free")+
-  theme_minimal()
+visualizeCasesDeaths.df <-
+  CTP_cleaned[, c(
+    "date",
+    "state",
+    "daily_casesFilled",
+    "daily_deathsFilled"
+  )]
 
-# view fraction positive and smoothened fraction positive
-ggplot(data=CTP_fractionPositive_long,aes(x=date,y=value,color=variable))+
-  geom_line()+
-  facet_wrap(~state, scales="free")+
-  theme_minimal()
+visualizeCasesDeaths.df.long <-
+  melt(visualizeCasesDeaths.df,
+       id.vars = c("date", "state"))
+
+visualizeFracPos.df <-
+  CTP_cleaned[, c("date",
+                  "state",
+                  "daily_fractionPositive",
+                  "daily_fractionPositive_15dayMovingAvg"
+                  )]
+visualizeFracPos.df.long <-
+  melt(visualizeFracPos.df, id.vars = c("date", "state"))
+
+# Arrange state/territory names in alphabetical order
+state_names_sort <- sort(state_names)
+
+# Initialize list to store plots
+plots <- vector(mode = "list", length = length(state_names_sort))
+
+for (i in 1:length(state_names)) {
+
+  cli_status_update(msg = paste("Producing graphs - ", state_names[i]))
+
+  visualizeCasesDeaths.df.long.state <-
+    visualizeCasesDeaths.df.long[which(visualizeCasesDeaths.df.long$state ==
+                                                     state_names_sort[i]), ]
+  plotCasesDeaths <-
+    ggplot(data = visualizeCasesDeaths.df.long.state, aes(x = date, y = value, color = variable)) +
+    geom_line(
+      data = filter(
+        visualizeCasesDeaths.df.long.state,
+        variable == "daily_casesFilled"
+      )
+    ) +
+    geom_line(
+      data = filter(
+        visualizeCasesDeaths.df.long.state,
+        variable == "daily_deathsFilled"
+      )
+    ) +
+    labs(title = paste0(state_names_sort[i], " Daily Counts")) +
+    theme_light() +
+    xlim(min(visualizeFracPos.df.long$date),
+         max(visualizeFracPos.df.long$date))
   
-# view daily deaths
-ggplot(data=CTP_movingAverages,aes(x=date,y=daily_deaths))+
-  geom_line()+
-  facet_wrap(~state, scales="free")+
-  theme_minimal()
+  visualizeFracPos.df.long.state <-
+    visualizeFracPos.df.long[which(visualizeFracPos.df.long$state == state_names_sort[i]), ]
+  plotFracPos <-
+    ggplot(data = visualizeFracPos.df.long.state, aes(x = date, y = value, color = variable)) +
+    geom_point(data = filter(
+      visualizeFracPos.df.long.state,
+      variable == "daily_fractionPositive"
+      ),
+      na.rm = TRUE
+    ) +
+    geom_line(
+      data = filter(
+        visualizeFracPos.df.long.state,
+        variable == "daily_fractionPositive_15dayMovingAvg"
+      )
+    ) +
+    labs(title = paste0(state_names_sort[i], " Daily Fraction Test Positive")) +
+    theme_light() +
+    xlim(min(visualizeFracPos.df.long$date),
+         max(visualizeFracPos.df.long$date)) +
+    ylim(0, 1)
+  
+  plots[[i]] <- ggpubr::ggarrange(plotCasesDeaths,
+                                  plotFracPos,
+                                  nrow = 2,
+                                  ncol = 1,
+                                  align = "v")
+  rm(plotCasesDeaths, plotFracPos)
+  
+}
+cli_process_done()
 
+#######################
+##                   ##
+## Save plots as PDF ##
+##                   ##
+#######################
+
+cli_process_start("Saving graphs")
+ggsave(
+  graphs_path,
+  marrangeGrob(grob = plots, nrow = 1, ncol = 1),
+  width  = 8.5,
+  height = 11
+)
+cli_process_done()
+cli_alert_info("Graphs were saved to {.file {graphs_path}}")
+
+warnings()
