aa66636f1ca9a0e63c7b098440108af2769d622a
Marcus Russi <marcusrussi@gmail.com>
2021-03-04 20:54:10 -0500

    WIP on NYT code

---
+++ R/cleanNYT-counties.R
@@ -0,0 +1,78 @@
+#!/usr/bin/env Rscript
+library(dplyr,     warn.conflicts = FALSE)
+library(readr,     warn.conflicts = FALSE)
+library(docopt,    warn.conflicts = FALSE)
+library(magrittr,  warn.conflicts = FALSE)
+library(cli,       warn.conflicts = FALSE)
+library(stringr,   warn.conflicts = FALSE)
+library(lubridate, warn.conflicts = FALSE)
+library(purrr,     warn.conflicts = FALSE)
+                    
+'NYT County-data Cleaner
+
+Usage:
+  cleanNYT-counties.R -o <path> [--writeRejects <path>] <path>
+  cleanNYT-counties.R (-h | --help)
+  cleanNYT-counties.R --version
+
+Options:
+  -o <path>              Path to output cleaned data to.
+  --writeRejects <path>  Path to output a .csv of rejected FIPS [fips, code, reason]
+  <path>                 NYTimes us-counties.csv file [date, county, state, fips, cases, deaths]
+  -h --help              Show this screen.
+  --version              Show version.
+
+' -> doc
+
+ps <- cli_process_start
+pd <- cli_process_done
+
+args <- docopt(doc, version = 'cleanNYT-counties 0.1')
+
+output_path  <- args$o
+data_path    <- args$path
+rejects_path <- args$writeRejects
+
+cols_only(
+  date   = col_date(format=""),
+  fips   = col_character(),
+  cases  = col_number(),
+  deaths = col_number()
+) -> colSpec
+
+ps("Reading case/death file {.file {data_path}}")
+d <- read_csv(data_path, col_types = colSpec)
+pd()
+
+cli_alert_info("Moving from cumulative counts to incidence")
+d <- arrange(d, fips, date) %>% group_by(fips) %>%
+  mutate(
+    # Can't have cases or deaths decrease, hence the max()
+    cases = pmax(cases - lag(cases, default = 0), 0),
+    deaths = pmax(deaths - lag(deaths, default = 0), 0)
+  )
+
+ps("Removing counties with fewer than 60 days' observations")
+
+startingFIPS <- unique(d$fips)
+shortFIPSStripped <- group_by(d, fips) %>% filter(n() > 60) %>% ungroup
+
+endingFIPS <- unique(shortFIPSStripped$fips)
+rejects <- tibble(
+  fips   = setdiff(startingFIPS, endingFIPS),
+  code   = 'UNDER60',
+  reason = "Fewer than 60 days of data"
+)
+pd()
+
+ps("Writing cleaned data to {.file {output_path}}")
+write_csv(shortFIPSStripped, output_path)
+pd()
+
+if (!identical(args$writeRejects, FALSE)) {
+  ps("Writing rejected counties to {.file {rejects_path}}")
+  write_csv(rejects, rejects_path)
+  pd()
+}
+
+warnings()
--- makefile
+++ makefile
@@ -2,6 +2,9 @@
 jhu_data  := data-sources/jhu-data/csse_covid_19_data/csse_covid_19_time_series
 jhu_reports := data-sources/jhu-data/csse_covid_19_data/csse_covid_19_daily_reports_us
 
+# Path to NYTimes submodule
+nyt := data-sources/nytimes-data
+
 # Short for data-products
 dp := data-products
 
@@ -47,3 +50,10 @@ $(dp)/jhu-states.csv $(dp)/jhu-states-rejects.csv: R/cleanJHU-states.R \
 	Rscript $< -o $(dp)/jhu-states.csv \
 	  --writeRejects $(dp)/jhu-states-rejects.csv \
 	  --reportsPath  $(jhu_reports)
+
+$(dp)/nytimes-counties.csv $(dp)/nytimes-counties-rejects.csv: R/cleanNYT-counties.R \
+  $(nyt)/us-counties.csv
+	@mkdir -p data-products/
+	Rscript $< -o $(dp)/nytimes-counties.csv \
+	  --writeRejects $(dp)/nytimes-counties-rejects.csv \
+	  $(nyt)/us-counties.csv
